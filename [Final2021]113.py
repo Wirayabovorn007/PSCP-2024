7a6803063e297a75891635bdcf33af1e5e222a93
Wiraya_ <154668128+Wirayabovorn007@users.noreply.github.com>
2024-10-15 21:24:18 +0700

    Add

---
+++ [Final2021]113.py
@@ -0,0 +1,10 @@
+'''c'''
+def main(num):
+    '''d'''
+    while True:
+        if '113' in num:
+            num = num.replace('113','')
+        else:
+            print(num)
+            break
+main(input())
